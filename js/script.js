3052162dd7b4710fdff871fb244df1db9e4b0d5c
Mahmoud Elsebaey <mahmoudelsebaey710@gmail.com>
2025-01-12 01:43:43 +0200

    adding search overlay section , update the navbar active nav

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
+//  ______________ Navbar Nav Links________________
  
+let allItemsLinks = document.querySelectorAll(`#nav .nav-item a`);
+for (let i = 0; i < allItemsLinks.length; i++) {
+  allItemsLinks[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+
+//  ______________ Search Overlay ________________
+let searchOverlay = document.querySelector(".search-overlay"),
+  searchBtnClose = document.querySelector(".search-overlay-btnClose");
+// ===> function to Close OR Open Overlay Window
+function showOverlay() {
+  searchOverlay.style.display === "none"
+    ? (searchOverlay.style.display = "block")
+    : (searchOverlay.style.display = "none");
+}
