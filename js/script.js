1a9231d4a6aad9a2db6693764a3d3b330b8f6a6c
Mahmoud Elsebaey <mahmoudelsebaey710@gmail.com>
2025-02-24 22:33:18 +0200

    add new section (Gallery)

--- js/script.js
+++ js/script.js
@@ -24,3 +24,32 @@ function showOverlay() {
     ? (searchOverlay.style.display = "block")
     : (searchOverlay.style.display = "none");
 }
+
+// class Car {
+//   constructor(name, model) {
+//     this.n = name;
+//     this.m = model;
+//   }
+//   info() {
+//     return `The Car ${this.n} that model is ${this.m}`;
+//   }
+// }
+
+// class Motor extends Car {
+//   #s;
+//   constructor(name, model, speed) {
+//     super(name, model);
+//     this.#s = speed;
+//   }
+//   info() {
+//     return `The Car ${this.n} that model is ${this.m} and speed ${this.s} km/h`;
+//   }
+// }
+
+// let car1 = new Car("BMW", 2019);
+// console.log(car1.info());
+
+// let motor1 = new Motor("Hommer", 2016 , 15);
+// console.log(motor1.info());
+
+// console.log(car1.__proto__ === Car.prototype);
